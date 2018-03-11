748a8dc8de19db25687657f732b48cc95c4e438c
Temi Lajumoke <temi@temilajumoke.com>
2018-03-11 17:18:33 +0100

    change firebase config

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,9 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDt3h4AlJHpt7Iu4DBPbM8HrsbHdAhTWfg",
-    authDomain: "finalyear-d451d.firebaseapp.com",
-    databaseURL: "https://finalyear-d451d.firebaseio.com",
-    projectId: "finalyear-d451d",
-    storageBucket: "finalyear-d451d.appspot.com",
-    messagingSenderId: "1087785280574"
-
+    apiKey: "AIzaSyDBQJ-1C6cl2ppCwPJ10byoOJgHIT-x814",
+    authDomain: "expensetracker-c77fd.firebaseapp.com",
+    databaseURL: "https://expensetracker-c77fd.firebaseio.com",
+    projectId: "expensetracker-c77fd",
+    storageBucket: "expensetracker-c77fd.appspot.com",
+    messagingSenderId: "867309100530",
 };
