8b0af54ced25a1fb3e8f1b25284002aa51d48d98
Temi Lajumoke <temi@temilajumoke.com>
2018-03-12 13:30:41 +0100

    refactor income list component

--- src/pages/income-list/income-list.ts
+++ src/pages/income-list/income-list.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController, } from 'ionic-angular';
-import{AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController, } from 'ionic-angular';
 import { IncomeListProvider } from '../../providers/income-list/income-list';
 import{Observable} from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 //import { ItemSliding } from 'ionic-angular';
 import { Slide } from 'ionic-angular/components/slides/slide';
+import{AngularFireDatabase, AngularFireAction, AngularFireList } from 'angularfire2/database';
+
 
 
 @IonicPage()
@@ -14,51 +15,68 @@ import { Slide } from 'ionic-angular/components/slides/slide';
   templateUrl: 'income-list.html',
 })
 export class IncomeListPage {
-  incom:any;
-  constructor( private alert: AlertController,  private afAuth:AngularFireAuth, private income:IncomeListProvider,private database: AngularFireDatabase,public navCtrl: NavController, public navParams: NavParams) {
-    this.income.getuserincome().then((res: any) => {
-      console.log(res);
-      this.incom= res;
-     })
+  expense: any;
+  userId: string;
+  userIncomeRef: AngularFireList<any>;
+  userIncome: Observable<any[]>;
+  constructor( private alert: AlertController, 
+    public loadingCtrl: LoadingController,
+    private afAuth:AngularFireAuth, private incomeProvider:IncomeListProvider,
+    public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  updateEx(inc: any, newValue) {
-    this.income.updateIn(inc,newValue);
-  }
-
-  delete(inc){
-    this.income.deleteIn(inc);
-  }
-
-  deleteIn(){
-      let alert = this.alert.create({
-
-        message: 'Are you sure you want to delete this item?',
-        buttons: [
-          {
-            text: 'Cancel',
-            role: 'cancel',
-            handler: () => {
-            // this.handleSlidingItems(slider);
-              //console.log('Cancel clicked');
-              
-            }
-          },
-          {
-            text: 'Ok',
-            handler: () => {
-              //console.log('Buy clicked');
-              this.delete;
-            }
-          }
-        ]
+  ionViewDidLoad() {
+    const loader = this.loadingCtrl.create({
+      content: "loading your income",
+    });
+    
+    loader.present().then(() => {
+      this.userIncomeRef = this.incomeProvider.getAllUserIncome(this.userId);
+      this.userIncome = this.userIncomeRef.snapshotChanges().map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
       });
-      alert.present();
-    }
+      loader.dismiss();
+    });
+  }
 
-    handleSlidingItems(slider) {
-      // Close any open sliding items when the page updates
-      slider.close();
-    }
+
+  // updateEx(inc: any, newValue) {
+  //   this.income.updateIn(inc,newValue);
+  // }
+
+  // delete(inc){
+  //   this.income.deleteIn(inc);
+  // }
+
+  // deleteIn(){
+  //     let alert = this.alert.create({
+
+  //       message: 'Are you sure you want to delete this item?',
+  //       buttons: [
+  //         {
+  //           text: 'Cancel',
+  //           role: 'cancel',
+  //           handler: () => {
+  //           // this.handleSlidingItems(slider);
+  //             //console.log('Cancel clicked');
+              
+  //           }
+  //         },
+  //         {
+  //           text: 'Ok',
+  //           handler: () => {
+  //             //console.log('Buy clicked');
+  //             this.delete;
+  //           }
+  //         }
+  //       ]
+  //     });
+  //     alert.present();
+  //   }
+
+  //   handleSlidingItems(slider) {
+  //     // Close any open sliding items when the page updates
+  //     slider.close();
+  //   }
 
 }
