ba7885395dd469b11c945335e774aa1465976d7b
Temi Lajumoke <temi@temilajumoke.com>
2018-03-12 17:17:41 +0100

    implement expense chart

--- src/pages/overview/overview.ts
+++ src/pages/overview/overview.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { ExpenseListProvider } from '../../providers/expense-list/expense-list';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { SigninPage } from '../signin/signin';
+import { Observable } from 'rxjs/Observable';
 
 /**
  * Generated class for the OverviewPage page.
@@ -14,15 +18,86 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'overview.html',
 })
 export class OverviewPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  userExpenses: Observable<any[]>;
+  totalUserExpenses: number;
+  userId: string;
+  foodExpenses: number;
+  educationExpenses: number;
+  entertainentExpenses: number;
+  transportExpenses: number;
+  clothingExpenses: number;
+  utilityExpenses: number;
+  isDataAvailable: boolean = false;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public loadingCtrl: LoadingController, public expenseProvider: ExpenseListProvider,
+    public afAuth: AngularFireAuth) {
+      afAuth.authState.subscribe(user => {
+        if (!user) {
+          this.navCtrl.push(SigninPage)
+        }
+        this.userId = user.uid;
+      });
   }
-  // Doughnut
-public doughnutChartLabels:string[] = ['Food', 'Entertainent', 'transport'];
-public doughnutChartData:number[] = [350, 450, 100];
-public doughnutChartType:string = 'doughnut';
+  doughnutChartLabels:string[] = ['Food', 'Entertainent', 'transport', 'utilities', 'education', 'clothes'];
+  doughnutChartData:number[];
+  doughnutChartType:string = 'doughnut';
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OverviewPage');
+    this.totalUserExpenses = 0;
+    this.educationExpenses = 0;
+    this.entertainentExpenses = 0;
+    this.transportExpenses = 0;
+    this.clothingExpenses = 0;
+    this.utilityExpenses = 0;
+    this.foodExpenses = 0;
+    
+    const loader = this.loadingCtrl.create({
+      content: "loading your personal finances",
+    });
+    
+    loader.present().then(() => {
+      this.userExpenses = this.expenseProvider.getAllUserExpenses(this.userId).valueChanges();
+    }).then(() => {
+      loader.dismiss();
+      this.userExpenses.subscribe(expenses => {
+        this.doughnutChartData = [];
+        expenses.forEach(expense => {
+          switch (expense.category) {
+            case "food":
+              this.foodExpenses += Number(expense.amount)
+              this.doughnutChartData[0] = this.foodExpenses;
+              break;
+            case "entertainent":
+              this.entertainentExpenses += Number(expense.amount)
+              this.doughnutChartData[1] = this.entertainentExpenses;              
+              break;
+            case "transportation":
+              this.transportExpenses += Number(expense.amount)
+              this.doughnutChartData[2] = this.transportExpenses;                            
+              break;
+            case "utilities":
+              this.utilityExpenses += Number(expense.amount)
+              this.doughnutChartData[3] = this.utilityExpenses;                                         
+              break;
+            case "education":
+              this.educationExpenses += Number(expense.amount)
+              this.doughnutChartData[4] = this.educationExpenses;                                        
+              break;
+            case "clothes":
+              this.clothingExpenses += Number(expense.amount)
+              this.doughnutChartData[5] = this.clothingExpenses;
+              break;
+            default:
+              break;
+          }
+        });
+        this.isDataAvailable = true;
+      })
+    })
+    .catch((error) => {
+      loader.setContent("couldn't get your personal finances");
+    });
   }
 
 }
